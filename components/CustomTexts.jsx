aae6e92fa21ff0cc2aee9a9cbe065f54d1e111c3
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-20 20:17:09 +0300

    Adds Explore section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -20,5 +20,12 @@ export const TypingText = ({ title, textStyles }) => (
 );
 
 export const TitleText = ({ title, textStyles }) => (
-  <h2>Title Text</h2>
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+  >
+    {title}
+  </motion.h2>
 );
