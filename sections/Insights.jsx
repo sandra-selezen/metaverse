2cd5dfec5bf711a1810d722ffbb05e0b22d9bdef
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-21 01:39:41 +0300

    Update Insights.jsx

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,8 +1,23 @@
 'use client';
 
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { TypingText, TitleText, InsightCard  } from "../components";
+import { insights } from "../constants";
+
 const Insights = () => (
-  <section>
-    Insights section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`} 
+    >
+      <TypingText title="| Insight" textStyles="text-center" />
+      <TitleText title={<>Insight about metaverse</>} textStyles="text-center" />
+    </motion.div>
   </section>
 );
 
