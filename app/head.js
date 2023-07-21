f356b0943483741daa6958741a2bfaa85a4c0d27
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-21 13:56:56 +0300

    Update head.js

--- app/head.js
+++ app/head.js
@@ -2,6 +2,7 @@ const Head = () => (
   <>
     <title>Metaverse</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="'Metaverse' landing page using React, Next.js, Framer-motion and Tailwindcss" />
     <link rel="icon" href="/favicon.ico" />
   </>
 );
